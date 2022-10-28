cbcebdc92012a2bf5e9e955400b636157b202e9f
micaelabovero <micaelabovero@gmail.com>
2022-10-27 23:39:34 -0300

    arreglandofooter

---
+++ js/footer.js
@@ -0,0 +1,14 @@
+ const footer = document.querySelector(".footer");
+const content = `
+<div class="redes-container">
+<ul>
+ <li><a href="#" class ="facebook"><i class="fab fa-facebook-f"></i></a></li>
+ <li><a href="#" class ="instagram"><i class="fab fa-instagram"></i></a></li>
+ <li><a href="#" class ="twitter"><i class="fab fa-twitter"></i></a></li> 
+ <li><a href="#" class ="pinterest"><i class="fab fa-pinterest-p"></i></a></li>
+  </ul> 
+  </div>
+
+
+`;
+footer.innerHTML = content;
